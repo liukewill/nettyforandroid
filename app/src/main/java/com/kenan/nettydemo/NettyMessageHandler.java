26d62006f2483bf339d1cbc1c4ea03c484e48436
liuke <liuke@iwaimai.baidu.com>
2018-03-29 17:46:57 +0800

    1.添加 主线程消息回调

--- app/src/main/java/com/kenan/nettydemo/NettyMessageHandler.java
+++ app/src/main/java/com/kenan/nettydemo/NettyMessageHandler.java
@@ -1,5 +1,7 @@
 package com.kenan.nettydemo;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 
 import com.kenan.nettyforandroid.NettyClient;
@@ -20,6 +22,9 @@ import org.jboss.netty.channel.MessageEvent;
  */
 
 public class NettyMessageHandler implements INettyHandler {
+
+    Handler handler=new Handler(Looper.getMainLooper());//消息回调到主线程
+
     @Override
     public void onConnectSuccess() {
 
@@ -34,26 +39,31 @@ public class NettyMessageHandler implements INettyHandler {
     public void onSuccess(ChannelHandlerContext ctx, MessageEvent e) {
         Log.i(NettyClient.TAG,e.getMessage().toString());
 
-        NettyResponse response=(NettyResponse)e.getMessage();
+        final NettyResponse response=(NettyResponse)e.getMessage();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                if(NettyRequestFactory.COMMOND.AUTH==response.getResult()){
+                    NettyRequestFactory.setTicket(response.getValue(NettyRequestFactory.CONSTANT.TICKET_KEY));
+                    Log.i(NettyClient.TAG,"AUTH-RESPONSE");
+                    //验证通过  开启心跳
+                    com.kenan.nettyforandroid.NettyClient.getInstance().startHeartBeat();
 
-        if(NettyRequestFactory.COMMOND.AUTH==response.getResult()){
-            NettyRequestFactory.setTicket(response.getValue(NettyRequestFactory.CONSTANT.TICKET_KEY));
-            Log.i(NettyClient.TAG,"AUTH-RESPONSE");
-            //验证通过  开启心跳
-            com.kenan.nettyforandroid.NettyClient.getInstance().startHeartBeat();
+                }
 
-        }
+                if(NettyRequestFactory.COMMOND.HEART_BEAT==response.getResult()){
+                    //心跳通过
+                    Log.i(NettyClient.TAG,"HB-RESPONSE");
+                }
 
-        if(NettyRequestFactory.COMMOND.HEART_BEAT==response.getResult()){
-            //心跳通过
-            Log.i(NettyClient.TAG,"HB-RESPONSE");
-        }
+                if(NettyRequestFactory.COMMOND.NEW_ORDER==response.getResult()){
+                    //业务逻辑编码
+                    Log.i(NettyClient.TAG,"NEW-ORDER-RESPONSE");
+                    NettyMessageSender.sendNewOrderResponse();
+                }
+            }
+        });
 
-        if(NettyRequestFactory.COMMOND.NEW_ORDER==response.getResult()){
-            //业务逻辑编码
-            Log.i(NettyClient.TAG,"NEW-ORDER-RESPONSE");
-            NettyMessageSender.sendNewOrderResponse();
-        }
     }
 
     @Override
